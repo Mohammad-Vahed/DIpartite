0a0c10c0984febb511da738cc878fe2333747035
Mohammad-Vahed <39237264+Mohammad-Vahed@users.noreply.github.com>
2018-10-09 16:33:00 +0900

    Update DIpartite.cxx

--- DIpartite.cxx
+++ DIpartite.cxx
@@ -381,7 +381,7 @@ class Computer {
 											
 					 if (ch!='\n'){				
 					 cout << ch ;
-					 if(j<100 and i<2000){
+					 if(j<1000 and i<2000){
 					 str4[i][j]=(ch);
                      j++;}
                     }
